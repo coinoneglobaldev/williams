cb34c2a72119cf4dc3cb7eb9346d3daeacf6dde2
Alan Varghese <alanvarghese2516@gmail.com>
2025-02-17 15:20:41 +0530

    Update app version to 4.0.2 in constants.dart and pubspec.yaml.

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,7 +6,7 @@ Color secondaryColor = Colors.grey.shade800;
 Color buttonColor = Colors.blue.shade900;
 Color textColor = Colors.white;
 
-const String appVersion = '1.0.3';
+const String appVersion = '4.0.2';
 
 String baseUrl = 'https://sysware-lasovrana.co.uk/lasov';
 
